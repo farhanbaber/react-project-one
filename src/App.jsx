8002794718551c58e4f211a51eeaa99ed16541c4
farhan baber <farhanbaber965@gmail.com>
2025-09-16 11:15:02 +0500

    done

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,9 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Hero from './hero'
-import Cards from './cards'
-import Footer from './footer' 
+import Hero from './Hero'
+import Cards from './Cards'
+import Footer from './Footer' 
 
 function App() {
   const [count, setCount] = useState(0)
